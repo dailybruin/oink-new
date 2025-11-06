333b1d6d4033662aef6473ed6f38fddc4aaf6e74
Seoungmin Kim <99856926+EricSeoungminKim@users.noreply.github.com>
2025-11-06 04:11:22 -0800

    basic setup with /admin endpoint
    1. imported whitenoise and applied in settings.py for the middleware - that allows us to call static files with the frontend.
    
    NOTE:
    by running
    python manage.py createsuperuser
    in shell, it will let us to create a new superuser account that has access to /admin page

--- oink_project/settings.py
+++ oink_project/settings.py
@@ -24,6 +24,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -80,6 +81,7 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [
