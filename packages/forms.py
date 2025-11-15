51cbea03a8f6f042a0db5aaa7c48ea440f467751
Shrika Andhe <shrikarandhe@gmail.com>
2025-11-14 23:48:46 -0800

    adding specific widget changes for each input

--- packages/forms.py
+++ packages/forms.py
@@ -7,5 +7,23 @@ class PackageForm(forms.ModelForm):
         model = Package
         fields = ['slug', 'description', 'google_drive_url', 'publish_date', 'category']
         widgets = {
-            'publish_date': forms.DateInput(attrs={'type': 'date'}),
+             'description': forms.TextInput(attrs={
+                 'class': 'custom-input',
+                 'placeholder': 'Brief description',
+                 'style':'width: 450px;',
+                 'rows': 3,
+             }),
+             'slug': forms.TextInput(attrs={
+                 'placeholder': 'Slug (e.g. sports.mbb.oregon)',
+                 'style':'width: 450px;',
+             }),
+             'google_drive_url': forms.TextInput(attrs={
+                 'placeholder': 'Google Drive URL',
+                 'style':'width: 450px;',
+             }),
+            'publish_date': forms.DateInput(attrs={
+                'type': 'date',
+                'style':'width: 450px;'
+             }),
+             
         }
